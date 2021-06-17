c89ff26176a6f65608f802e412d59cae3baa5804
h4dri <adrianmalik333@gmail.com>
2021-06-17 15:31:22 +0200

    Class of vehicle interacting with weather conditions, pavement type, section delay, wind direction and force.

--- src/convoy/vehicle/Vehicle.java
+++ src/convoy/vehicle/Vehicle.java
@@ -6,28 +6,84 @@ public class Vehicle {
     private float vehiclePosition;
     private float carVelocity;
     private float fuelLevel;
+    private float maxVelocity;
+    private boolean fuelReserve;
 
     public Vehicle(int vehicleNumber, int routeSectionNumber, float fuelLevel,
-                   float carVelocity, float vehiclePosition){
+                   float carVelocity, float vehiclePosition, float maxVelocity) {
 
         this.vehicleNumber = vehicleNumber;
         this.routeSectionNumber = routeSectionNumber;
         this.fuelLevel = fuelLevel;
         this.carVelocity = carVelocity;
         this.vehiclePosition = vehiclePosition;
+        this.maxVelocity = maxVelocity;
+        this.fuelReserve = false;
     }
 
-    public void drive(float previousVehiclePosition, float expectedDistance){
+    public void drive(float previousVehiclePosition, float expectedDistance, int typeOfWeather, int typeOfRoute,
+                      float windDirectionX, float windDirectionY, float windForce, boolean isSectionDelayed) {
         float distance = previousVehiclePosition - this.vehiclePosition;
-        this.carVelocity = (distance - (distance/expectedDistance) - expectedDistance*1.01f);
-        this.fuelLevel = (this.fuelLevel - Math.abs(this.carVelocity)*0.01f);
+        this.carVelocity = (distance - (distance / expectedDistance) - expectedDistance * 1.01f);
+        this.fuelLevel = (this.fuelLevel - Math.abs(this.carVelocity) * 0.01f);
         this.vehiclePosition = this.vehiclePosition + this.carVelocity;
+        weatherDelay(typeOfWeather);
+        routeDelay(typeOfRoute);
+        windDelay(windDirectionX, windDirectionY, windForce);
+        if (this.carVelocity > this.maxVelocity) this.carVelocity = this.maxVelocity;
+        if (this.fuelLevel < 10.0f) fuelReserve = true;
+        if (isSectionDelayed) sectionDelay();
     }
-    public void drive(float convoyVelocity){
+
+    public void drive(float convoyVelocity, int typeOfWeather, int typeOfRoute, float windDirectionX,
+                      float windDirectionY, float windForce, boolean isSectionDelayed) {
         this.carVelocity = 0;
         this.carVelocity = convoyVelocity + this.carVelocity;
-        this.fuelLevel = (this.fuelLevel - this.carVelocity*0.01f - Math.abs(this.carVelocity)*0.01f);
+        this.fuelLevel = (this.fuelLevel - this.carVelocity * 0.01f - Math.abs(this.carVelocity) * 0.01f);
         this.vehiclePosition = this.vehiclePosition + this.carVelocity;
+        weatherDelay(typeOfWeather);
+        routeDelay(typeOfRoute);
+        windDelay(windDirectionX, windDirectionY, windForce);
+        if (this.carVelocity > this.maxVelocity) this.carVelocity = this.maxVelocity;
+        if (isSectionDelayed) sectionDelay();
+    }
+
+    public void sectionDelay() {
+        this.carVelocity = 0;
+    }
+
+    public void weatherDelay(int typeOfWeather) {
+        this.carVelocity = this.carVelocity - this.carVelocity * (1 - (float) Math.pow(typeOfWeather, 2) / 10);
+    }
+
+    public void routeDelay(int typeOfRoute) {
+        this.carVelocity = this.carVelocity - this.carVelocity * (1 - (float) Math.pow(typeOfRoute, 2) / 10);
+    }
+
+    public void windDelay(float windDirectionX, float windDirectionY, float windForce) {
+        if (windForce > 1.0f || windForce < 0.0f) {
+            System.out.println("Wind force shall be 0.0 to 1.0");
+        } else {
+            if (windDirectionX < 0) {
+                if (windDirectionY > 0) {
+                    this.carVelocity = this.carVelocity + this.carVelocity * windForce / 4;
+                } else if (windDirectionY < 0) {
+                    this.carVelocity = this.carVelocity - this.carVelocity * windForce / 4;
+                }
+            } else if (windDirectionX > 0) {
+                if (windDirectionY > 0) {
+                    this.carVelocity = this.carVelocity + this.carVelocity * windForce / 4;
+                } else if (windDirectionY < 0) {
+                    this.carVelocity = this.carVelocity - this.carVelocity * windForce / 4;
+                }
+            } else if (windDirectionX == 0) {
+                if (windDirectionY > 0) {
+                    this.carVelocity = this.carVelocity + this.carVelocity * windForce / 2;
+                } else if (windDirectionY < 0) {
+                    this.carVelocity = this.carVelocity - this.carVelocity * windForce / 2;
+                }
+            }
+        }
     }
 
     public int getVehicleNumber() {
@@ -48,5 +104,13 @@ public class Vehicle {
 
     public float getFuelLevel() {
         return fuelLevel;
+    }
+
+    public float getMaxVelocity() {
+        return maxVelocity;
+    }
+
+    public boolean isFuelReserve() {
+        return fuelReserve;
     }
 }
