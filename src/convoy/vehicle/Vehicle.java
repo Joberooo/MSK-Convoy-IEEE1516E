397a89ae65b1d098083fe169afa0950ff1e9d6ef
h4dri <adrianmalik333@gmail.com>
2021-06-17 13:32:07 +0200

    Edit Vehicle Class

--- src/convoy/vehicle/Vehicle.java
+++ src/convoy/vehicle/Vehicle.java
@@ -1,12 +1,12 @@
 package convoy.vehicle;
 
 public class Vehicle {
-    int vehicleNumber;
-    int routeSectionNumber;
-    float vehiclePosition;
-    float carVelocity;
-    float carAcceleration;
-    float fuelLevel;
+    private final int vehicleNumber;
+    private int routeSectionNumber;
+    private float vehiclePosition;
+    private float carVelocity;
+    private float carAcceleration;
+    private float fuelLevel;
 
     public Vehicle(int vehicleNumber, int routeSectionNumber, float fuelLevel,
                    float carVelocity, float vehiclePosition){
@@ -18,12 +18,11 @@ public class Vehicle {
         this.vehiclePosition = vehiclePosition;
     }
 
-    public void drive(float previousVehiclePosition, float convoyVelocity, float expectedDistance){
+    public void drive(float previousVehiclePosition, float expectedDistance){
         float distance = previousVehiclePosition - this.vehiclePosition;
         this.carAcceleration = (float) (distance - (distance/expectedDistance) - expectedDistance*1.01);
-        this.carVelocity = convoyVelocity + this.carAcceleration;
-        this.fuelLevel = (float) (this.fuelLevel - this.carVelocity*0.01 - Math.abs(this.carAcceleration)*0.01);
-        this.vehiclePosition = this.vehiclePosition + this.carVelocity;
+        this.fuelLevel = (float) (this.fuelLevel - Math.abs(this.carAcceleration)*0.01);
+        this.vehiclePosition = this.vehiclePosition + this.carAcceleration;
     }
     public void drive(float convoyVelocity){
         this.carAcceleration = 0;
@@ -36,11 +35,23 @@ public class Vehicle {
         return vehicleNumber;
     }
 
-    public double getVehiclePosition() {
+    public int getRouteSectionNumber() {
+        return routeSectionNumber;
+    }
+
+    public float getVehiclePosition() {
         return vehiclePosition;
     }
 
-    public int getRouteSectionNumber() {
-        return routeSectionNumber;
+    public float getCarVelocity() {
+        return carVelocity;
+    }
+
+    public float getCarAcceleration() {
+        return carAcceleration;
+    }
+
+    public float getFuelLevel() {
+        return fuelLevel;
     }
 }
